c9fc24239a128e49ceed2586b78956f5612216e1
Julien TASSIN <julien.tassin.pro@gmail.com>
2025-08-19 00:10:51 +0200

    add next analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Dancing_Script, Crimson_Text } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -66,6 +67,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${dancingScript.variable} ${crimsonText.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
